692cc3ba19a258a0d05ed5031ea5995095da02bc
Joaquin Rivera Lopez <joaquin.rivera@gsdynamics.com>
2017-01-26 17:11:07 -0500

    Add files via upload

---
+++ otrosunidadmedida.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<?php
+session_start();
+require('funciones.php');
+conexion();
+
+if(isset($_SESSION['login'])){
+?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Waka-s Textiles Finos S.A.</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/Formularios.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="mainAdmin.php">Waka-s</a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Registros<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="gestionCV.php">Visualizaci&oacuten de Confirmaciones de Venta</a></li>
+                        <li><a href="gestionOP.php">Visualizaci&oacuten de Ordenes de Producci&oacuten</a></li>
+                        <li><a href="rendimiento.php">Visualizaci&oacuten de Rendimiento</a></li>
+                        <li><a href="gestionProductos.php">Visualizaci&oacuten de Productos</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Operaciones<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="nuevaCV.php">Nueva Confirmaci&oacuten de Venta</a></li>
+                        <li><a href="nuevaHE.php">Nueva Hoja de Especificaciones</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Informaci&oacuten Interna<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="gestionMateriales.php">Materiales</a></li>
+                        <li><a href="gestionMaquinas.php">M&aacutequinas</a></li>
+                        <li><a href="gestionInsumos.php">Insumos</a></li>
+                        <li><a href="gestionOperarios.php">Empleados</a></li>
+                        <li><a href="gestionProcesos.php">Procesos</a></li>
+                        <li><a href="gestionRepuestos.php">Repuestos</a></li>
+                        <li><a href="menuagregarotros.php">Otros</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contactos<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="gestionClientes.php">Clientes</a></li>
+                        <li><a href="gestionProveedores.php">Proveedores</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
+
+<section class="container">
+    <div>
+        <h3>Agregar Unidad de Medida</h3>
+    </div>
+    <form action="otrosunidadmedida.php" method="post">
+        <div class="form-group">
+            <div>
+                <label for="idunimed">Unidad de Medida:</label>
+            </div>
+            <div>
+                <input type="text" id="idunimed" name="idunimed"/>
+            </div>
+        </div>
+        <div>
+            <input class="btn btn-success"type="submit" name="guardar" value="Agregar"/>
+            <input formaction="menuagregarotros.php" class="btn btn-danger" type="submit" value="Regresar">
+        </div>
+    </form>
+
+    <?php
+    if(isset($_POST['guardar'])){
+        $agregar="INSERT INTO unidadmedida(idUnidadMedida) VALUES ('".$_POST['idunimed']."')";
+        $agregar1=mysql_query($agregar);
+        if ( !empty( $error = mysql_error() ) )
+        {
+            echo 'Mysql error '. $error ."<br />\n";
+        }else{
+            echo "<br>";
+            echo "<div class='alert alert-success' role='alert'>";
+            echo 	"<p> <strong>Unidad de Medida Agregada Exitosamente</strong></p>";
+            echo " </div>";
+        }
+    }
+    ?>
+</section>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
+
+<?php
+}else{
+    echo "Alguien esta tratando de entrar a nuestro sitio Web. Un log ha sido creado automaticamente para despedirte. Gracias por visitar Waka-s SGI :)";
+}
+?>
