261efe8bef6bebae266a0d3229ee7ef81a23ed95
IgnacioRondon <ignacio.rondon@gsdynamics.com>
2017-01-31 13:57:44 -0500

    Update get_ciudad.php

--- get_ciudad.php
+++ get_ciudad.php
@@ -5,8 +5,8 @@ conexion();
 
 if(!empty($_POST["pais_id"])) {
     $ciudad ="SELECT * FROM Ciudad WHERE idPais = '" . $_POST["pais_id"] . "'";
-    $result=runQuery($ciudad);
-    foreach($result as $fila) {
+    $result=mysql_query($ciudad);
+    while($fila=mysql_fetch_array($result)) {
        echo "
             <option value=".$fila["idCiudad"].">".$fila["nombre"]."</option>
        ";
