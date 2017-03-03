cba044dbdf567185ed1ca339b63478580f74bb4e
IgnacioRondon <ignacio.rondon@gsdynamics.com>
2017-03-03 00:02:37 -0500

    Add files via upload

--- gestionClientes.php
+++ gestionClientes.php
@@ -160,15 +160,15 @@ if(isset($_SESSION['login'])){
                     </div>
                     <div class="col-sm-7">
                         <select id="buscar" name="filtroCliente" class="ddselect-12">
-				<option>Seleccionar</option>
-				<?php
+                            <option>Seleccionar</option>
+                            <?php
                                 $datos=selectTable("Cliente");
                                 while($opcion=mysql_fetch_array($datos)){
                                     echo "
                                         <option>".$opcion['nombre']."</option>
                                     ";
                                 }
-                            	?>
+                            ?>
                         </select>
                     </div>
                 </div>
