7e35fcf2fd0ded9c60aaaa8f6da08b0389c2c035
IgnacioRondon <ignacio.rondon@gsdynamics.com>
2017-04-08 13:09:49 -0500

    Update menuagregarotros.php

--- menuagregarotros.php
+++ menuagregarotros.php
@@ -119,7 +119,7 @@ mysql_query("SET NAMES 'utf8'");
                     <button class="btn btn-default col-sm-12 boton" type="submit" formaction="otrostalla.php">Agregar Talla</button>
                 </div>
                 <div class="col-sm-12">
-                    <button class="btn btn-default col-sm-12 boton" type="submit" formaction="otrostipotalla.php">Agregar Tipo de Talla</button>
+                    <button class="btn btn-default col-sm-12 boton" type="submit" formaction="otrostipotalla.php">Agregar Códificacion de Talla</button>
                 </div>
             </form>
         </section>
