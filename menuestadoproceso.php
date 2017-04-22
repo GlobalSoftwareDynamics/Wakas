5aa57b25e039919ced2b2246c489e6beeaa0b0fc
IgnacioRondon <ignacio.rondon@gsdynamics.com>
2017-04-22 02:55:02 -0500

    Add files via upload

---
+++ menuestadoproceso.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<?php
+session_start();
+require('funciones.php');
+conexion();
+
+if(isset($_SESSION['login'])){
+mysql_query("SET NAMES 'utf8'");
+?>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Waka-s Textiles Finos S.A.</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <link href="css/Tablas.css" rel="stylesheet">
+    <link href="css/Formularios.css" rel="stylesheet">
+
+</head>
+
+<body>
+<header>
+    <nav class="navbar navbar-inverse">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="mainAdmin.php" id="brand">W<span class="alfa">&alpha;</span>k<span class="alfa">&alpha;</span>-s</a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Registros<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="gestionCV.php">Visualizaci&oacuten de Confirmaciones de Venta</a></li>
+                            <li><a href="gestionOP.php">Visualizaci&oacuten de Ordenes de Producci&oacuten</a></li>
+                            <li><a href="rendimiento.php">Visualizaci&oacuten de Rendimiento</a></li>
+                            <li><a href="menuestadoproceso.php">Visualizaci&oacuten de Estado de Proceso</a></li>
+                            <li><a href="gestionProductos.php">Visualizaci&oacuten de Productos</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Operaciones<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="nuevaCV.php">Nueva Confirmaci&oacuten de Venta</a></li>
+                            <li><a href="nuevaHE.php">Nueva Hoja de Especificaciones</a></li>
+                            <li><a href="OPnueva.php">Nueva Orden de Producción</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Informaci&oacuten Interna<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="gestionMateriales.php">Materiales</a></li>
+                            <li><a href="gestionMaquinas.php">M&aacutequinas</a></li>
+                            <li><a href="gestionInsumos.php">Insumos</a></li>
+                            <li><a href="gestionOperarios.php">Empleados</a></li>
+                            <li><a href="gestionProcesos.php">Procesos</a></li>
+                            <li><a href="gestionRepuestos.php">Repuestos</a></li>
+                            <li><a href="menuagregarotros.php">Otros</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contactos<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="gestionClientes.php">Clientes</a></li>
+                            <li><a href="gestionProveedores.php">Proveedores</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </nav>
+</header>
+
+<section class="container">
+    <form action="#" method="post" class="form-horizontal jumbotron col-sm-8 col-sm-offset-2">
+        <div>
+            <h3>Seguimiento de Lotes</h3>
+        </div>
+        <hr>
+        <div class="form-group">
+            <div class="col-sm-5">
+                <label for="op" class="formlabels col-sm-12">Orden de Producción:</label>
+            </div>
+            <div class="col-sm-7">
+                <input type="text" name="idop" id="op" class="textinput-8">
+            </div>
+        </div>
+        <hr>
+        <div class="form-group">
+            <div class="col-sm-12">
+                <div class="col-sm-6">
+                    <input formaction="regproceexterno.php" class="btn btn-success col-sm-10 col-sm-offset-1" type="submit" name="regproceexter" value="Registrar Proceso Tercerizado">
+                </div>
+                <div class="col-sm-6">
+                    <input formaction="estadoordenprod.php" class="btn btn-primary col-sm-10 col-sm-offset-1" type="submit" name="revestado" value="Revisar Estado">
+                </div>
+            </div>
+        </div>
+    </form>
+</section>
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
+
+<?php
+}else{
+    echo "Usted no está autorizado para ingresar a esta sección. Por favor vuelva a la página de inicio de sesión e identifíquese.";
+}
+?>
