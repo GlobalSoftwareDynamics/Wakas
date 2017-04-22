38871d867f401ef2543e349333121bfdaf60beb2
IgnacioRondon <ignacio.rondon@gsdynamics.com>
2017-04-22 03:22:50 -0500

    Add files via upload

---
+++ seguimientoproducto.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<?php
+session_start();
+require('funciones.php');
+conexion();
+
+if(isset($_SESSION['login'])){
+mysql_query("SET NAMES 'utf8'");
+?>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Waka-s Textiles Finos S.A.</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/Tablas.css" rel="stylesheet">
+    <link href="css/Formatos.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-inverse">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="mainAdmin.php" id="brand">W<span class="alfa">&alpha;</span>k<span class="alfa">&alpha;</span>-s</a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Registros<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="gestionCV.php">Visualizaci&oacuten de Confirmaciones de Venta</a></li>
+                            <li><a href="gestionOP.php">Visualizaci&oacuten de Ordenes de Producci&oacuten</a></li>
+                            <li><a href="rendimiento.php">Visualizaci&oacuten de Rendimiento</a></li>
+                            <li><a href="gestionProductos.php">Visualizaci&oacuten de Productos</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Operaciones<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="nuevaCV.php">Nueva Confirmaci&oacuten de Venta</a></li>
+                            <li><a href="nuevaHE.php">Nueva Hoja de Especificaciones</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Informaci&oacuten Interna<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="gestionMateriales.php">Materiales</a></li>
+                            <li><a href="gestionMaquinas.php">M&aacutequinas</a></li>
+                            <li><a href="gestionInsumos.php">Insumos</a></li>
+                            <li><a href="gestionOperarios.php">Empleados</a></li>
+                            <li><a href="gestionProcesos.php">Procesos</a></li>
+                            <li><a href="gestionRepuestos.php">Repuestos</a></li>
+                            <li><a href="menuagregarotros.php">Otros</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contactos<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="gestionClientes.php">Clientes</a></li>
+                            <li><a href="gestionProveedores.php">Proveedores</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </nav>
+</header>
+<?php
+$lote=strtoupper ($_POST["idlote"]);
+?>
+<section class='container-fluid'>
+    <div class='col-sm-1'>
+        <div>
+            <img width='110' height='110' src='image/logowakas.jpg'/>
+        </div>
+    </div>
+    <div class='col-sm-6' id='descripcionbrand'>
+        <div>
+            <span id='brand'>W<span class='alfa'>&alpha;</span>k<span class='alfa'>&alpha;</span>-s <span id='subbrand'>Textiles Finos SAC</span></span>
+        </div>
+        <div>
+            <span>Urb. Francisco Mostajo G-25</span>
+        </div>
+        <div>
+            <span>04002 Arequipa - Peru</span>
+        </div>
+    </div>
+    <div class='col-sm-4'>
+        <div class='titulo'>
+            <h3>Detalle de Lote</h3>
+        </div>
+        <div>
+            <h4 class='desctitulo'>
+                <?php
+                echo $lote;
+                ?>
+            </h4>
+        </div>
+    </div>
+</section>
+<hr>
+<section class="container">
+    <div class="col-sm-12">
+        <label for='avance'>Avance del Lote:</label>
+        <span id='avance'><?php echo $_POST['avance']?></span>
+    </div>
+    <hr>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>Fecha</th>
+                <th>Empleado</th>
+                <th>Procedimiento</th>
+                <th>Cantidad</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+        $result=selectOrderedTableWhere('EmpleadoLote','idLote',"'".$lote."'",'fecha');
+        while ($fila=mysql_fetch_array($result)){
+            echo "<tr>";
+            echo "<td>".$fila['fecha']."</td>";
+            $result1=selectTableWhere('Empleado','idEmpleado',"'".$fila['idEmpleado']."'");
+            while ($fila1=mysql_fetch_array($result1)){
+                echo "<td>".$fila1['nombres']." ".$fila1['apellidos']."</td>";
+            }
+            $result2=selectTableWhere('SubProceso','idProcedimiento',"'".$fila['idProcedimiento']."'");
+            while ($fila2=mysql_fetch_array($result2)){
+                echo "<td>".$fila2['descripcion']."</td>";
+            }
+            echo "<td>".$fila['cantidad']."</td>";
+            echo "</tr>";
+        }
+        ?>
+        </tbody>
+    </table>
+</section>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
+<?php
+}else{
+    echo "Usted no está autorizado para ingresar a esta sección. Por favor vuelva a la página de inicio de sesión e identifíquese.";
+}
+?>
